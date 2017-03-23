f3f8d5b5c6a090592bf589f3bb2c3d3ddf616e3a
Adam Caron <adamwcaron@gmail.com>
2017-03-23 15:52:59 -0600

    Profit. Provisions instances to aws given a Terraform configuration in the frontend, such as that on https://www.terraform.io/intro/getting-started/build.html (add your own credentials)

--- routes/instances.js
+++ routes/instances.js
@@ -1,10 +1,27 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express')
+const router = express.Router()
+const nodeCmd = require('node-cmd')
+const fs = require('fs')
 
-/* GET home page. */
-router.post('/create', function(req, res, next) {
-  // res.render('index', { title: 'Express' });
-  res.json(req.body.terraformConfig)
-});
+/* POST create instance. */
+router.post('/create', (req, res, next) => {
+  if (!fs.existsSync('./data/deployments')) { // ensure '/deployments' exists
+    fs.mkdirSync('./data/deployments')
+  }
 
-module.exports = router;
+  const id = Math.round(Math.random() * 1000000000000000000)
+  const dir = `./data/deployments/${id}`
+  fs.mkdirSync(dir) // namespece deployment files
+  fs.writeFile(`${dir}/instance.tf`, req.body.terraformConfig, (err) => { // create tf file
+    if (err) res.status(500).json(err)
+    nodeCmd.get(`cd ${dir} && terraform plan`, (data, err) => { // run terraform plan
+      if (err) res.status(500).json(err)
+      nodeCmd.get(`cd ${dir} && terraform apply`, (data, err) => { // run terraform apply
+        if (err) res.status(500).json(err)
+        res.json(data)
+      })
+    })
+  })
+})
+
+module.exports = router
